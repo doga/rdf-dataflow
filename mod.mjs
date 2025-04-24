c1be97048473f0b950501c3254ff3df04d08b472
Doğa Armangil <doga.armangil@alumni.epfl.ch>
2025-04-24 06:54:41 +0200

    add default export

--- mod.mjs
+++ mod.mjs
@@ -1,3 +1,10 @@
-export { toDataset } from "./lib/parse.mjs";
-export { fromDataset } from "./lib/serialise.mjs";
-export { contentType } from "./lib/content-type.mjs";
+
+// TODO add isDataflowObject(obj) for fast validity test
+
+import { toDataset } from "./lib/parse.mjs";
+import { fromDataset } from "./lib/serialise.mjs";
+import { contentType } from "./lib/content-type.mjs";
+
+export default {toDataset, fromDataset, contentType };
+export {toDataset, fromDataset, contentType };
+
